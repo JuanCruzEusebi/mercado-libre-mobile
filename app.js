4f92084306a0e52567c5ccc541b3788be15a8b65
unknown <you@example.com>
2022-08-15 19:27:26 -0300

    Adaptando a heroku

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const publicPath = path.resolve(__dirname, "./public")
 app.use(express.static(publicPath));
 
 
-app.listen('3000', () => console.log('server running'));
+// app.listen('3000', () => console.log('server running'));
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server running")
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/index.html'))
